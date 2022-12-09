cf4a2a7a18178e59b4b48ce77c6ad99df2b88504
markonovakovic <graornovakovic@gmail.com>
2022-12-09 21:03:39 +0100

    part one done

--- src/main/kotlin/day2/Day2.kt
+++ src/main/kotlin/day2/Day2.kt
@@ -4,23 +4,51 @@ import readInput
 
 typealias Round = Pair<Play, Play>
 
-fun Round(s: List<String>): Round {
+val Round.player
+    get() = second
+
+private fun Round(s: List<String>): Round {
     val (opponent, player) = s
     return Round(Play(opponent), Play(player))
 }
 
-sealed interface Play
-object Rock : Play
-object Paper : Play
-object Scissors : Play
+private val Round.score
+    get() = Result(this).score + player.score
 
-fun Play(s: String) =
+sealed class Play(val score: Int)
+object Rock : Play(1)
+object Paper : Play(2)
+object Scissors : Play(3)
+
+sealed class Result(val score: Int)
+object Win : Result(6)
+object Draw : Result(3)
+object Loss : Result(0)
+
+private fun Result(round: Round) =
+    when (round) {
+        Rock to Paper -> Win
+        Rock to Scissors -> Loss
+        Paper to Scissors -> Win
+        Paper to Rock -> Loss
+        Scissors to Rock -> Win
+        Scissors to Paper -> Loss
+        Rock to Rock -> Draw
+        Paper to Paper -> Draw
+        Scissors to Scissors -> Draw
+        else -> throw IllegalArgumentException("invalid round: $round")
+    }
+
+private fun Play(s: String) =
     when (s) {
         "A", "X" -> Rock
         "B", "Y" -> Paper
         "C", "Z" -> Scissors
         else -> throw IllegalArgumentException("illegal play :$s")
     }
+
+fun playStrategy(fileName: String) =
+    readStrategy(fileName).sumOf(Round::score)
 
 fun readStrategy(fileName: String) =
     readInput(fileName)
@@ -29,5 +57,4 @@ fun readStrategy(fileName: String) =
         .map(::Round)
 
 private fun String.splitIntoRounds() = split('\n')
-
 private fun String.splitIntoPlays() = split(' ')
--- src/test/kotlin/Day2Test.kt
+++ src/test/kotlin/Day2Test.kt
@@ -1,6 +1,7 @@
 import day2.Paper
 import day2.Rock
 import day2.Scissors
+import day2.playStrategy
 import day2.readStrategy
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -10,11 +11,26 @@ class Day2Test {
     @Test
     fun `parse input`() {
         val strategy = readStrategy("day2/test_input.txt")
+
         val expectedStrategy = listOf(
             Rock to Paper,
             Paper to Rock,
             Scissors to Scissors,
         )
         assertEquals(expectedStrategy, strategy)
+    }
+
+    @Test
+    fun `play test game`() {
+        val score = playStrategy("day2/test_input.txt")
+
+        assertEquals(15, score)
+    }
+
+    @Test
+    fun `play game (part one)`() {
+        val score = playStrategy("day2/input.txt")
+
+        assertEquals(12458, score)
     }
 }
