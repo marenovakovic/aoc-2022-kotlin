7331a5a77714f30506b5ba588b8bdb1fa2f3650b
markonovakovic <graornovakovic@gmail.com>
2023-06-06 20:09:55 +0200

    reformatting

--- src/main/kotlin/day3/Day3.kt
+++ src/main/kotlin/day3/Day3.kt
@@ -2,13 +2,17 @@ package day3
 
 import readInput
 
-fun compartments(s: String) = s.substring(0, s.length / 2) to s.substring(s.length / 2)
+fun compartments(s: String) =
+    s.substring(0, s.length / 2) to s.substring(s.length / 2)
 
-fun priority(c: Char) = if (c.isLowerCase()) c.code - 96 else (c.code - 65 + 27)
+fun priority(c: Char) =
+    if (c.isLowerCase()) c.code - 96 else (c.code - 65 + 27)
 
-fun priority(chars: List<Char>) = chars.sumOf { priority(it) }
+fun priority(chars: List<Char>) =
+    chars.sumOf { priority(it) }
 
-fun commonItems(pair: Pair<String, String>) = pair.first.toSet().intersect(pair.second.toSet()).toList()
+fun commonItems(pair: Pair<String, String>) =
+    pair.first.toSet().intersect(pair.second.toSet()).toList()
 
 fun readTestInput() =
     readInput("day3/test_input.txt").lines()
