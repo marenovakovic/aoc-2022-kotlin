913498b7ded4f862f4a0f57cef65379811ccfdb2
markonovakovic <graornovakovic@gmail.com>
2023-06-07 18:36:37 +0200

    String -> IntRange. it might be easier to operate on range and to find assignments that contain other assignments

--- src/main/kotlin/day4/Day4.kt
+++ src/main/kotlin/day4/Day4.kt
@@ -2,7 +2,8 @@ package day4
 
 typealias Assignment = String
 
-fun parseSection(assignment: Assignment): Pair<String, String> =
+fun parseSection(assignment: Assignment): Pair<IntRange, IntRange> =
     assignment
         .split(',')
+        .map { it.first().digitToInt()..it.last().digitToInt() }
         .let { it.first() to it.last() }
--- src/test/kotlin/Day4Test.kt
+++ src/test/kotlin/Day4Test.kt
@@ -5,9 +5,9 @@ import io.kotest.matchers.equals.shouldBeEqual
 class Day4Test : DescribeSpec({
     context("section") {
         it("can parse assignment") {
-            parseSection("2-4,6-8") shouldBeEqual ("2-4" to "6-8")
-            parseSection("1-3,5-7") shouldBeEqual ("1-3" to "5-7")
-            parseSection("8-9,9-9") shouldBeEqual ("8-9" to "9-9")
+            parseSection("2-4,6-8") shouldBeEqual (2..4 to 6..8)
+            parseSection("1-3,5-7") shouldBeEqual (1..3 to 5..7)
+            parseSection("8-9,9-9") shouldBeEqual (8..9 to 9..9)
         }
     }
 })
