0bb25219a534fef3e0d9aa9305efd2530bb98df6
markonovakovic <graornovakovic@gmail.com>
2023-06-07 18:39:23 +0200

    typealias Assignment = IntRange

--- src/main/kotlin/day4/Day4.kt
+++ src/main/kotlin/day4/Day4.kt
@@ -1,8 +1,9 @@
 package day4
 
 typealias PairAssignment = String
+typealias Assignment = IntRange
 
-fun parseSection(pairAssignment: PairAssignment): Pair<IntRange, IntRange> =
+fun parseSection(pairAssignment: PairAssignment): Pair<Assignment, Assignment> =
     pairAssignment
         .split(',')
         .map { it.first().digitToInt()..it.last().digitToInt() }
