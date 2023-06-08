4e515a9bc200f0e5df33322fa83a0146468c0c02
markonovakovic <graornovakovic@gmail.com>
2023-06-08 18:08:13 +0200

    parsing assignments like in aoc video on Kotlin YT channel and that somehow solved the issue...

--- src/main/kotlin/day4/Day4.kt
+++ src/main/kotlin/day4/Day4.kt
@@ -5,16 +5,13 @@ import readInput
 typealias PairAssignment = Pair<Assignment, Assignment>
 typealias Assignment = IntRange
 
-private val String.firstDigit get() = first().digitToInt()
-private val String.lastDigit get() = last().digitToInt()
-
 fun String.toPairAssignment() =
     split(',')
         .map(::sectionDigits)
         .let(::pairAssignment)
 
 private fun sectionDigits(s: String) =
-    s.firstDigit..s.lastDigit
+    s.substringBefore('-').toInt()..s.substringAfter('-').toInt()
 
 private fun pairAssignment(assignments: List<Assignment>) =
     assignments.first() to assignments.last()
