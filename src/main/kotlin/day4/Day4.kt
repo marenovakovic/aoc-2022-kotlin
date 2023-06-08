30c3c9ee0c319f7c3d26e9d3dc8023a01aeed7ef
markonovakovic <graornovakovic@gmail.com>
2023-06-08 13:43:59 +0200

    overlap + refactoring

--- src/main/kotlin/day4/Day4.kt
+++ src/main/kotlin/day4/Day4.kt
@@ -1,10 +1,13 @@
 package day4
 
-typealias PairAssignment = String
+typealias PairAssignment = Pair<Assignment, Assignment>
 typealias Assignment = IntRange
 
-fun parseSection(pairAssignment: PairAssignment): Pair<Assignment, Assignment> =
-    pairAssignment
+fun parseSection(line: String): PairAssignment =
+    line
         .split(',')
         .map { it.first().digitToInt()..it.last().digitToInt() }
         .let { it.first() to it.last() }
+
+fun overlaps(a: Assignment, b: Assignment) =
+    a.intersect(b).isNotEmpty()
--- src/test/kotlin/Day4Test.kt
+++ src/test/kotlin/Day4Test.kt
@@ -1,3 +1,4 @@
+import day4.overlaps
 import day4.parseSection
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.equals.shouldBeEqual
@@ -8,6 +9,13 @@ class Day4Test : DescribeSpec({
             parseSection("2-4,6-8") shouldBeEqual (2..4 to 6..8)
             parseSection("1-3,5-7") shouldBeEqual (1..3 to 5..7)
             parseSection("8-9,9-9") shouldBeEqual (8..9 to 9..9)
+        }
+        it("overlap") {
+            overlaps(2..4, 2..3) shouldBeEqual true
+            overlaps(3..3, 2..8) shouldBeEqual true
+            overlaps(2..4, 3..5) shouldBeEqual true
+            overlaps(2..4, 5..7) shouldBeEqual false
+            overlaps(5..7, 1..3) shouldBeEqual false
         }
     }
 })
