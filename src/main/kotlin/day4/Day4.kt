7b7055d94a65649bf74fcaf245e159ed3f952b08
markonovakovic <graornovakovic@gmail.com>
2023-06-08 13:52:44 +0200

    refactoring

--- src/main/kotlin/day4/Day4.kt
+++ src/main/kotlin/day4/Day4.kt
@@ -3,10 +3,16 @@ package day4
 typealias PairAssignment = Pair<Assignment, Assignment>
 typealias Assignment = IntRange
 
-fun String.toPairAssignment(): PairAssignment =
+private val String.firstDigit get() = first().digitToInt()
+private val String.lastDigit get() = last().digitToInt()
+
+fun String.toPairAssignment() =
     split(',')
-        .map { it.first().digitToInt()..it.last().digitToInt() }
-        .let { it.first() to it.last() }
+        .map(::sectionDigits)
+        .let(::pairAssignment)
+
+private fun sectionDigits(s: String) = s.firstDigit..s.lastDigit
+private fun pairAssignment(assignments: List<Assignment>) = assignments.first() to assignments.last()
 
 fun overlaps(a: Assignment, b: Assignment) =
     a.intersect(b).isNotEmpty()
